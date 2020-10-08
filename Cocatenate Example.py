195d1b32651e5017aaf1b47df3c7d2ce73ef8049
Victor <72521293+LogatVic@users.noreply.github.com>
2020-10-08 17:18:55 +0300

    Add files via upload

---
+++ Cocatenate Example.py
@@ -0,0 +1,13 @@
+print("THE EXAMPLE OF CONCATENATION IN PYTHON")
+input("Input 'A' in order to receive part 1: ")
+str1 = A = 'DEFINITION: Concatenating in the Python'
+print("Concatenating in the Python...")
+input("Input 'B' in order to receive part 2: ")
+str2 = B = 'means obtaining a new string that contains both of the original strings.'
+print("means obtaining a new string that contains both of the original strings.")
+
+print(str1+" "+str2)
+
+print("Do not forget it ever!")
+
+
