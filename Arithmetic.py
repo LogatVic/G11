c85393069999f3c0be57c04c6af9c30e399c9ab6
Victor <72521293+LogatVic@users.noreply.github.com>
2020-10-08 17:20:24 +0300

    Add files via upload

---
+++ Arithmetic.py
@@ -0,0 +1,22 @@
+Print("Zero as operation symbol"
+      "\nprogram stop")
+while True:
+    s = input("arithmetic operation (+,-,*,/): ")
+    if s == '0':
+        break
+    if s in ('+', '-', '*', '/'):
+        x = float(input("x="))
+        y = float(input("y="))
+        if s == '+':
+            print("%.2f" % (x+y))
+        elif s == '-':
+            print("%.2f" % (x-y))
+        elif s == '*':
+            print("%.2f" % (x*y))
+        elif s == '/':
+            if y != 0:
+                print("%.2f" % (x/y))
+            else:
+                print("Division by Zero!")
+    else:
+        print("Invalid operation sign!")
